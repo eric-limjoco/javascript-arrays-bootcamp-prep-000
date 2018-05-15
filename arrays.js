6dd50a1e7b17846be6de112d2653dc44bb412728
ericlimjoco@gmail.com Limjoco <ericlimjoco@gmail.com>
2018-05-15 22:51:10 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,9 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
+
+function addElementToBeginningOfArrray(array, el){
+  return array.unshift(el)
+}
+
+function destructivelyAddElementToBeginningOfArrray(array, el){
+  return array.unshift(el)
+}
